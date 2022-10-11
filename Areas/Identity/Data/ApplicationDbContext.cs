de025eb1c275f1221ec0fbbb772d7d8a7efdfbbf
mariuszpaluch2001 <mariuszpaluch001@gmail.com>
2022-10-11 11:06:31 +0100

    Add ApplicationUserEntityConfiguration.

--- Areas/Identity/Data/ApplicationDbContext.cs
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DiscussionForum.Areas.Identity.Data;
 
@@ -18,5 +19,17 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
     }
 }
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(255);
+        builder.Property(x => x.LastName).HasMaxLength(255);
+    }
+}
+
